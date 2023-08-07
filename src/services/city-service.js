a5f2fcbdb228c3372a81f1c2455125a1b15ac705
leander006 <leanderdsilva06@gmail.com>
2023-08-07 20:06:58 +0530

    Added city controller and service

---
+++ src/services/city-service.js
@@ -0,0 +1,43 @@
+const { CityRepository } = require("../models/index");
+
+class CityService {
+  constructor() {
+    this.CityRepository = new CityRepository();
+  }
+
+  async createCity(data) {
+    try {
+      const result = await this.CityRepository.createCity(data);
+      return result;
+    } catch (error) {
+      throw { error };
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      const result = await this.CityRepository.deleteCity(cityId);
+      return result;
+    } catch (error) {
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const result = await this.CityRepository.updateCity(cityId, data);
+      return result;
+    } catch (error) {
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const result = await this.CityRepository.getCity(cityId);
+      return result;
+    } catch (error) {
+      throw { error };
+    }
+  }
+}
